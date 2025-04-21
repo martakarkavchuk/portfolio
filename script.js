3653bbd4915ad34073b1182743f2da9be3ee72a0
martakarkavchuk <karkavcukmartusa@gmail.com>
2025-04-21 14:09:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,19 +10,37 @@ function updateCarousel() {
   track.style.transform = `translateX(-${slideWidth * currentSlide}px)`;
 }
 
+// Next button functionality
 nextButton.addEventListener('click', () => {
-  currentSlide = (currentSlide + 1) % slides.length;
+  currentSlide = (currentSlide + 1) % slides.length; // Loop back to the first slide
   updateCarousel();
 });
 
+// Previous button functionality
 prevButton.addEventListener('click', () => {
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length; // Loop back to the last slide
   updateCarousel();
 });
 
-// Optional: auto-play
-setInterval(() => {
+// Auto-play functionality (change slide every 5 seconds)
+let autoPlay = setInterval(() => {
   currentSlide = (currentSlide + 1) % slides.length;
   updateCarousel();
 }, 5000);
 
+// Reset auto-play on user interaction
+nextButton.addEventListener('click', () => {
+  clearInterval(autoPlay); // Stop auto-play
+  autoPlay = setInterval(() => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    updateCarousel();
+  }, 5000); // Restart auto-play after user clicks next
+});
+
+prevButton.addEventListener('click', () => {
+  clearInterval(autoPlay); // Stop auto-play
+  autoPlay = setInterval(() => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    updateCarousel();
+  }, 5000); // Restart auto-play after user clicks prev
+});
